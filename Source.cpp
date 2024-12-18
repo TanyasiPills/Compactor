5039850d2ef4f870ae8f4e194632f7acf0e112e2
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2024-12-18 21:01:10 +0100

    something

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,16 @@
-#include<bitset>
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <bitset>
 
 int main() {
-	int n = 345;
-	std::bitset<64> bits(n);
-	std::cout << bits << std::endl;
-	std::string nm;
-	std::cin >> n;
+	std::string filename = "test.txt";
+	std::ifstream file(filename, std::ios::binary);
+
+	if (!file.is_open()) {
+		std::cout << "shit is hitting the fan" << std::endl;
+		return 1;
+	}
+
+	std::vector<unsigned char> buffer((std::istreambuf_iterator<char>)(file), std::istreambuf_iterator<char>());
 }
